872e2b68ec6637394b5a127708abe9f119f7dc7d
heolyun <hl620@naver.com>
2026-01-02 11:27:16 +0900

    SELECT JOIN 설명 2

--- Quiz11.sql
+++ Quiz11.sql
@@ -1,2 +1,26 @@
 --Quiz 01
 
+select * from student;
+
+select name,JUMIN,BIRTHDAY,
+    CASE 
+        WHEN SUBSTR(birthday,4,2) IN('01','02','03') THEN '1분기'
+        WHEN SUBSTR(birthday,4,2) IN('04','05','06') THEN '2분기'
+        WHEN SUBSTR(birthday,4,2) IN('07','08','09') THEN '3분기'
+        WHEN SUBSTR(birthday,4,2) IN('10','11','12') THEN '4분기'
+    END as 분기
+from student;
+
+--Quiz 02
+
+SELECT * FROM emp;
+
+select empno,ename,sal,
+    CASE
+        WHEN sal BETWEEN 1 AND 1000 THEN 'LEVEL 1'
+        WHEN sal BETWEEN 1001 AND 2000 THEN 'LEVEL 2'
+        WHEN sal BETWEEN 2001 AND 3000 THEN 'LEVEL 3'
+        WHEN sal BETWEEN 3001 AND 4000 THEN 'LEVEL 4'
+        WHEN sal>4001 THEN 'LEVEL 5'
+    END 급여등급
+from emp;
--- select08.sql
+++ select08.sql
@@ -45,3 +45,89 @@ where s.profno = p.profno(+);
 select *
 from student s LEFT OUTER JOIN professor p
 ON s.profno = p.profno;
+
+--INNER
+select *
+from professor p, student s
+where p.profno = s.profno;
+
+--OUTER
+select *
+from professor p, student s
+where p.profno = s.profno(+);
+
+--학생 20 교수 16, INNER JOIN 연결가능 15
+select *
+from professor p, student s
+where s.profno = p.profno(+);
+--OUTER JOIN 할때는 N:1 의 관계로 JOIN 해줘라
+
+select * from department;
+SELECT * FROM student;
+SELECT * FROM professor;
+
+SELECT s.name , s.deptno1, d.dname 
+FROM student s, department d
+where s.deptno1 = d. deptno;
+
+SELECT * FROM student s , professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno;
+
+SELECT s.name, p.name, d.dname
+FROM
+    student s INNER JOIN professor p
+    ON s.profno = p.profno
+    INNER JOIN department d
+    ON s.profno = p.profno;
+    
+SELECT s.name, p.name, d.dname
+FROM student s , professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno
+AND s.deptno1 = 101;
+
+1) 학생명, 학생학과번호, 학생의 학과명, 교수멍 ,교수학과번호
+SELECT s.name, s.deptno1, d.dname, p.name, p.deptno
+FROM student s , professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno;
+
+2) 학생명, 학생학과번호, 교수의 학과명, 교수멍 ,교수학과번호
+SELECT s.name, s.deptno1, d.dname, p.name, p.deptno
+FROM student s , professor p, department d
+where s.profno = p.profno
+AND p.deptno = d.deptno;
+
+2) 학생명, 학생학과번호, 학생의 학과명, 교수멍 ,교수학과번호, 교수의 학과명
+SELECT s.name, s.deptno1, ds.dname , p.name , p.deptno,dp.dname
+FROM student s, professor p, department ds , department dp
+where s.profno = p.profno -- 학생과 <ㅡ> 지도교수연결
+and s.deptno1 = ds.deptno -- 학생학과<-> 학과정보
+and p.deptno = dp.deptno -- 교수학과 <-> 학과정보
+;
+
+-----------------------------------------------------
+INNER JOIN OUTER JOIN
+등가조인 =    비등가 조인
+
+SELECT * FROM customer;
+SELECT * FROM gift;
+
+SELECT * 
+FROM customer c, gift g
+where c.point BETWEEN g.g_start and g.g_end;
+
+SELECT * FROM hakjum;
+
+SELF JOIN
+
+SELECT * FROM emp;
+
+select e1.empno, e1.ename 사원이름, e1.mgr, e2.empno , e2.ename 상사이름
+from emp e1, emp e2
+where e1.mgr = e2.empno;
+
+select a.empno, a.name , a.pempno, b.empno , b.name
+from emp2 a, emp2 b
+where a.pempno = b.empno;
